d6a5438491eaf1a1c131feb6439faf122459ffb6
Tuanhao <tuanhao256@gmail.com>
2017-06-01 22:59:25 -0400

    fixed vietnamese version and added more words

--- function.js
+++ function.js
@@ -1,7 +1,11 @@
 var engArray = ["Apple", "Bus", "Teacher", "Juice", "Cat", "Airplane", "Blue", "Plate", "honesty" , "equality" , "First place",
-"Action", "Hapiness", "Earth", "Secret", "City", "Emotion", "Crime", "Anxiety", "Nature"]
+"Action", "Hapiness", "Earth", "Secret", "City", "Emotion", "Crime", "Anxiety", "Nature", "Soap", "Machine", "To dance", "Just right",
+"Habit", "Expirience", "Strength", "Shape", "Color", "Taste", "Meeting", "Coast/ Beach", "Price", "Novel", "Manager", "Future",
+"Usually", "Free", "Salary", "Color", "Zoo", "Grape", "Caution", "Police", "Relationship", "Research", "Sound"]
 var japArray = ["りんご", "バス", "先生", "ジュース", "ねこ", "ひこうき", "あおい", "おさら", "しょうじき" , "びょうどう", "いちばん",
-"こうどう", "こうふく", "ちきゅう", "ひみつ", "とし", "かんじょう", "はんざい", "ふあん", "しぜん"]
+"こうどう", "こうふく", "ちきゅう", "ひみつ", "とし", "かんじょう", "はんざい", "ふあん", "しぜん", "せっけん", "きかい", "おどります", "ちょうどいい",
+"しゅうかん", "けいけん", "ちから", "かたち", "いろ", "あじ", "かい", "かいがん", "ねだん", "しょうせつ", "かんりにん", "しょうらい",
+"たいてい", "むりょう", "きゅうりょう", "いろ", "どうぶつえん", "ぶどう", "ちゅうい", "けいさつ", "かんけい", "けんきゅう", "おと"]
 var cardLocation = []
 var wordsChoosingArray = []
 var wordsArray = []
@@ -14,7 +18,7 @@ cardFilling()
 
 /** Create an array with 8 random numbers within engArray vs japArray length */
 function wordsChoosing() {
-  var randomNum = Math.floor(Math.random() * 21)
+  var randomNum = Math.floor(Math.random() * engArray.length)
   if (wordsChoosingArray.length == 8) {
     wordsArrayFilling()
     return
@@ -98,15 +102,22 @@ function cardRotation(card) {
   When card is flipped, it is not clickable
 */
 function flipCard(card) {
-  card.style.animation = "flipCard 1.25s linear"
+  card.style.animation = "flipCard 1s linear"
+  setTimeout(function() {
+    card.style.backgroundImage = "none"
+  }, 500)
   card.style.animationFillMode = "forwards"
   card.style.pointerEvents = "none"
 }
 
 /**  By assigning animation style to close card */
 function closeCard(cards) {
-  cards[0].style.animation = "closeCard 1.25s linear"
-  cards[1].style.animation = "closeCard 1.25s linear"
+  setTimeout(function() {
+    cards[0].style.backgroundImage = "url('./card.png')"
+    cards[1].style.backgroundImage = "url('./card.png')"
+  }, 500)
+  cards[0].style.animation = "closeCard 1s linear"
+  cards[1].style.animation = "closeCard 1s linear"
 }
 
 /** win function by showing an gif */
@@ -117,8 +128,13 @@ function win() {
 
 /** Vietnamese word change */
 function changeToVietnamese() {
-  engArray = ["Táo", "Buýt", "Cô-Giáo", "Nước ép", "Mèo", "Máy-bay", "Màu-xanh", "Đĩa"]
+  engArray = ["Táo", "Xe buýt", "Giáo viên", "Nước ép", "Mèo", "Máy bay", "Màu xanh", "Đĩa", "Thành thật" , "Công bằng" , "Hạng nhất",
+  "Hành động", "Niềm vui", "Trái Đất", "bí mật", "Thành phố", "Cảm súc", "Tội phạm", "Nỗi lo âu", "Tự nhiên", "xà phòng", "Máy móc", "Nhảy nhót", "Vừa đủ",
+  "Thói quen", "Kinh nghiệm", "Sức mạnh", "Hình dáng", "Màu sắc", "Mùi vị", "Cuộc họp", "Bờ biển", "Giá tiền", "Tiểu thuyết", "Quản lý", "Tương lai",
+  "Thường xuyên", "Miễn phí", "Lương bổng", "Màu sắc", "Sở thú", "Nho", "Cẩn thận", "Cảnh sát", "Mối quan hệ", "Nghiên cứu", "Âm thanh"]
+  wordsArray = []
   clearBoard()
+  wordsArrayFilling()
   cardFilling()
   document.getElementsByTagName('p')[0].innerHTML = "Hãy tìm những cặp từ đồng nghĩa để chiến thắng"
   alert("Chuyển đổi thành công")
